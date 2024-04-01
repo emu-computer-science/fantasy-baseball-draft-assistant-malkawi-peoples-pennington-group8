ff9e21bc601de854af23bf6264afb3677ade5c19
cashflow72 <justintpeoples@gmail.com>
2024-03-31 17:14:59 -0700

    Added pitcher class and array List
    Added pOverall functionality

--- DraftSystem.java
+++ DraftSystem.java
@@ -5,100 +5,139 @@ import java.io.*;
 
 public class DraftSystem {
 	private ArrayList<BaseBallPlayer> baseBallPlayers;
-	
-	private ArrayList<BaseBallPlayer> teamA,teamB,teamC,teamD;
-	
+	private ArrayList<Pitcher> pitchers;
+
+	private ArrayList<BaseBallPlayer> teamA, teamB, teamC, teamD;
+
 	public DraftSystem() {
-		//initialize empty arrayLists for teams
+		// initialize empty arrayLists for teams
 		teamA = new ArrayList<>();
 		teamB = new ArrayList<>();
 		teamC = new ArrayList<>();
 		teamD = new ArrayList<>();
-		
-		//to do:parse the file so that it cuts up the contents into seperate string looking for a (char num num) sequence
-		//To Do: Read the PlayerList.txt file into baseBallPlayers
+
+		// to do:parse the file so that it cuts up the contents into seperate string
+		// looking for a (char num num) sequence
+		// To Do: Read the PlayerList.txt file into baseBallPlayers
+		pitchers = new ArrayList<>();
 		baseBallPlayers = new ArrayList<>();
 		String fullPlayerList = "";
 		try {
+			// baseball player file
 			Scanner fileReader = new Scanner(new File("PlayerList.txt"));
-			while(fileReader.hasNextLine()) {
+			while (fileReader.hasNextLine()) {
 				fullPlayerList += fileReader.nextLine();
-				
+
 			}
-			//System.out.println(fullPlayerList);
+			// System.out.println(fullPlayerList);
+
+			String[] playerStrings = fullPlayerList.split("(?<=[a-zA-Z]{3}\\d{2})");
+
+			for (String playerInfo : playerStrings) {
+				baseBallPlayers.add(new BaseBallPlayer(playerInfo));
+			}
 			
-		String[] playerStrings = fullPlayerList.split("(?<=[a-zA-Z]{3}\\d{2})");
-		
-		for(String playerInfo : playerStrings) {
-			baseBallPlayers.add(new BaseBallPlayer(playerInfo));
-		}
+			// pitcher file
+			fileReader = new Scanner(new File("PitcherList.txt"));
+			fullPlayerList = "";
+			while (fileReader.hasNextLine()) {
+				fullPlayerList += fileReader.nextLine();
+
+			}
+			// System.out.println(fullPlayerList);
+
+			playerStrings = fullPlayerList.split("(?<=[a-zA-Z]{3}\\d{2})");
+
+			for (String pitcherInfo : playerStrings) {
+				System.out.println(pitcherInfo);
+				pitchers.add(new Pitcher(pitcherInfo));
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Error creating Draft");
 			e.printStackTrace();
 		}
-				
-		
+
 	}
-	
-	//To Do: Check if the position of wanted player is already in team
+
+	// To Do: Check if the position of wanted player is already in team
 	public void iDraft(String lastName, char firstInitial) {
-		
-		for(BaseBallPlayer player : baseBallPlayers) {
+
+		for (BaseBallPlayer player : baseBallPlayers) {
 			String playerLast = player.getLastName();
 			char playerFirstInitial = player.getFirstName().charAt(0);
-			
-			if(lastName.equalsIgnoreCase(playerLast) && Character.toUpperCase(firstInitial) == playerFirstInitial) {
-				if(player.getIsDrafted()) {
+
+			if (lastName.equalsIgnoreCase(playerLast) && Character.toUpperCase(firstInitial) == playerFirstInitial) {
+				if (player.getIsDrafted()) {
 					System.out.println("Player already drafted");
-				}else {
+				} else {
 					teamA.add(player);
 					System.out.println(player.getLastName() + "has been drafted to team A");
 				}
 				break;
-							
+
 			}
 		}
-		
+
 	}
-	
-	
+
 	public void oDraft() {
 		System.out.println("This will draft players to teams eventually");
 	}
+
 	public void printTeam() {
 		System.out.println("This will print teams eventually");
 	}
+
 	public void printStars() {
 		System.out.println("This will eventually print teams in drafted order");
 	}
+
 	public void overall(String position) {
-		//print all players if empty -- if not, print only if positions are the same
-		//Sort by batting average (TEMPORAY)
+		// print all players if empty -- if not, print only if positions are the same
+		// Sort by batting average (TEMPORAY)
 		ArrayList<BaseBallPlayer> tempList = baseBallPlayers;
-		
+
 		tempList.sort(Comparator.comparingDouble(player -> player.getBA()));
-		
-		if(position.isEmpty()) {
-			for(BaseBallPlayer player : tempList) {
+
+		if (position.isEmpty()) {
+			for (BaseBallPlayer player : tempList) {
 				System.out.println(player.toString() + player.getBA());
 			}
-		}else {
-			for(BaseBallPlayer player : tempList) {
-				if(position.equalsIgnoreCase(player.getPosition())) {
+		} else {
+			for (BaseBallPlayer player : tempList) {
+				if (position.equalsIgnoreCase(player.getPosition())) {
 					System.out.println(player.toString() + player.getBA());
 				}
-				
+
 			}
 		}
 		System.out.println("This will print players eventually");
 	}
+
 	public void pOverall() {
-		System.out.println("This will print pitchers eventually");
+		// only printing top 50 pitchers
+		ArrayList<Pitcher> tempList = pitchers;
+
+		tempList.sort(Comparator.comparingDouble(player ->  ((Pitcher) player).getSO()).reversed());
+
+		int count = 0;
+		for (Pitcher player : tempList) {
+			if(count == 50) {
+				break;
+			}
+			System.out.println(player.toString() + player.getSO());
+			count++;
+
+		}
+
+		System.out.println("This will print players eventually");
 	}
+
 	public void evalFun() {
 		System.out.println("This will set the evaluation funaction for overall() eventually");
 	}
+
 	public void pEvalFun() {
 		System.out.println("This will set the evaluation function for pOverall() eventually");
 	}
--- Pitcher.java
+++ Pitcher.java
@@ -1,15 +1,16 @@
 
-public class BaseBallPlayer {
+public class Pitcher {
 	private String firstName;
 	private String lastName;
 	private boolean isDrafted;
-	private String playerPosition;
+	private String playerPosition = "P";
 	//maybe a hashset instead or something ??
 	private String teamName;
 	
-	private double rK,age,g,pA,aB,r,h,b2,b3,hR,rBI,sB,cS,bB,sO,bA,oBP,sLG,oPS,oPSPlus,tB,gDP,hBP,sH,sF,iBB, position;
+	private double sO;
 	
-	public BaseBallPlayer(String playerInfo) {
+	
+	public Pitcher(String playerInfo) {
 		String[] info = playerInfo.split(",");
 		
 		//fill blank stats with 0's
@@ -37,18 +38,10 @@ public class BaseBallPlayer {
 			lastName = lastName.substring(0, lastName.length() - 1);
 		}
 		
-		bA = Double.parseDouble(info[17]);
-		
+		sO = Double.parseDouble(info[21]);
 		teamName = info[3];	
 		
-		//find first position of an int and change it into correct playerPosition
-		playerPosition = info[28];
-		for(int i = 0; i < playerPosition.length(); i++) {
-			if(Character.isDigit(playerPosition.charAt(i))) {
-				findPosition(playerPosition.charAt(i));
-				break;
-			}
-		}
+		
 		
 		//System.out.println(firstName + "  " + lastName + "  " + teamName + "  " + playerPosition + "   "+ bA);
 		//System.out.println();
@@ -60,46 +53,12 @@ public class BaseBallPlayer {
 	public String toString() {
 		return (firstName + "        " + lastName + "        " + teamName + "        " + playerPosition + "        ");
 	}
-	private void findPosition(char posNum) {
-		// TODO Auto-generated method stub
-		switch(posNum) {
-		case '1' :
-			playerPosition = "P";
-			break;
-		case '2' :
-			playerPosition = "C";
-			break;
-		case '3' :
-			playerPosition = "1B";
-			break;
-		case '4' :
-			playerPosition = "2B";
-			break;
-		case '5' :
-			playerPosition = "3B";
-			break;
-		case '6' :
-			playerPosition = "SS";
-			break;
-		case '7' :
-			playerPosition = "LF";
-			break;
-		case '8' :
-			playerPosition = "CF";
-			break;
-		case '9' :
-			playerPosition = "RF";
-			break;
-		}
-	}
-
+	
 	
 	public void setIsDrafted() {
 		isDrafted = true;
 	}
-	public Double getBA() {
-		return bA;
-	}
+	
 	public String getPosition() {
 		return playerPosition;
 	}
@@ -120,5 +79,11 @@ public class BaseBallPlayer {
 	public boolean getIsDrafted() {
 		// TODO Auto-generated method stub
 		return isDrafted;
+	}
+
+
+	public double getSO() {
+		// TODO Auto-generated method stub
+		return sO;
 	}
 }
