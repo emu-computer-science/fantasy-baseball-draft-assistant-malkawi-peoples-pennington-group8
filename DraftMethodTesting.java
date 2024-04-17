5bbe759dcf485b1a9e5877d99eae700e7825df75
cashflow72 <justintpeoples@gmail.com>
2024-04-16 21:34:16 -0400

    added draft methods

--- DraftMethodTesting.java
+++ DraftMethodTesting.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 
 class DraftMethodTesting {
-	/*
 	@Test
 	void testOverallMethods() {
 		DraftSystem dS = new DraftSystem();
@@ -57,28 +56,9 @@ class DraftMethodTesting {
 		
 		
 		
-		dS.pOverall();
-		dS.evalFun("hR + r");
-		dS.overall("RF");
-		dS.pEvalFun("sO + hR");
-		dS.pOverall();
-		
-		
 	
 	}
-	*/
 	
-	@Test
-	void evalTest(){
-		DraftSystem dS = new DraftSystem();
-		
-		dS.overall("RF");
-		dS.evalFun("bA * r");
-		dS.overall("RF");
-		dS.pEvalFun("sO + hR");
-		dS.pOverall();
-	}
-	/*
 	@Test
 	void testDraftMethods() {
 		DraftSystem dS = new DraftSystem();
@@ -86,39 +66,45 @@ class DraftMethodTesting {
 		dS.iDraft("Hamilton", 'C');
 		dS.iDraft("Bannon", 'R');
 		dS.iDraft("Gallo", 'J');
-		
 		dS.iDraft("Gordon", 'N');
-		
-		
 		dS.iDraft("Gausman", 'K');
 		dS.iDraft("Eflin", 'Z');
 		dS.iDraft("Gray", 'S');
 		dS.iDraft("Bibee", 'T');
 		dS.iDraft("Abreu", 'B');
-		
 		dS.iDraft("Gausman", 'K');
 		dS.iDraft("Brito", 'J');
-		
 		dS.iDraft("Bannon", 'R');
 		
-		dS.oDraft("Cowser", 'C', 'A');
-		
+		dS.oDraft("Cowser", 'C', 'A');	
 		dS.oDraft("Bride", 'J', 'B');
 		dS.oDraft("Hamilton", 'D', 'B');
-		dS.oDraft("Hummel", 'C', 'B');
-		
+		dS.oDraft("Hummel", 'C', 'B');		
 		dS.oDraft("Blanco", 'D', 'C');
-		dS.oDraft("Capel", 'C', 'C');
-		
+		dS.oDraft("Capel", 'C', 'C');	
 		dS.oDraft("Capel", 'C', 'D');
 		dS.oDraft("Dalbec", 'B', 'D');
 		
 		dS.stars('A');
+		dS.stars('B');
+		
 		dS.team('A');
 		dS.team('B');
 		dS.team('C');
 		dS.team('D');
 		
 	}
-	*/
+	
+	@Test
+	void testEvalMethods() {
+		DraftSystem dS = new DraftSystem();
+		
+		dS.overall("");
+		dS.evalFun("bA * r");
+		dS.overall("");
+		
+		dS.pOverall();
+		dS.pEvalFun("iP * sO");
+		dS.pOverall();
+	}
 }
